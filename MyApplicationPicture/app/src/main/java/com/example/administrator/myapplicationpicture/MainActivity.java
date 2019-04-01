83f887ad44f1148ed45ef5fd5fb7deb493dcd99d
林境威 <u0424066@nkfust.edu.tw>
2019-04-01 12:48:38 +0800

    updata 1

--- MyApplicationPicture/app/src/main/java/com/example/administrator/myapplicationpicture/MainActivity.java
+++ MyApplicationPicture/app/src/main/java/com/example/administrator/myapplicationpicture/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.administrator.myapplicationpicture;
 
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        String Imagepath="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Usaf.e3sentry.750pix.jpg/580px-Usaf.e3sentry.750pix.jpg";
+        ImageLoaderConfiguration config=new ImageLoaderConfiguration.Builder(this).build();
+        final ImageView imageView=findViewById(R.id.imageView);
+        DisplayImageOptions options=new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .build();
+        ImageLoader.getInstance().loadImage(Imagepath,options,new SimpleImageLoadingListener(){
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                super.onLoadingComplete(imageUri, view, loadedImage);
+                imageView.setImageBitmap(loadedImage);
+            }
+        } );
+        }
 }
